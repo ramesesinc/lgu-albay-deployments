63b9f90b19ebe89527e9675da2b7e3adf45a6ccb
worgflores <worgflores@gmail.com>
2021-03-26 11:34:42 +0800

    _res: patches: updated

--- _res/patches/_initial/10-etracs-patch-province.sql
+++ _res/patches/_initial/10-etracs-patch-province.sql
@@ -333,3 +333,101 @@ INSERT INTO `sys_wf_transition` (`parentid`, `processname`, `action`, `to`, `idx
 INSERT INTO `sys_wf_transition` (`parentid`, `processname`, `action`, `to`, `idx`, `eval`, `properties`, `permission`, `caption`, `ui`) VALUES ('approver', 'subdivision', 'approve', 'provapprover', '90', NULL, '[caption:\'Approve\', confirm:\'Approve?\', messagehandler:\"default\", closeonend:false]', NULL, NULL, NULL);
 INSERT INTO `sys_wf_transition` (`parentid`, `processname`, `action`, `to`, `idx`, `eval`, `properties`, `permission`, `caption`, `ui`) VALUES ('provapprover', 'subdivision', 'backforprovapproval', 'approver', '95', NULL, '[caption:\'Cancel Posting\', confirm:\'Cancel posting record?\']', NULL, NULL, NULL);
 INSERT INTO `sys_wf_transition` (`parentid`, `processname`, `action`, `to`, `idx`, `eval`, `properties`, `permission`, `caption`, `ui`) VALUES ('provapprover', 'subdivision', 'completed', 'end', '100', NULL, '[caption:\'Approved\', visible:false]', NULL, NULL, NULL);
+
+
+
+CREATE TABLE `syncdata` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NULL,
+  `action` varchar(50) NOT NULL,
+  `dtfiled` datetime NOT NULL,
+  `orgid` varchar(50) NULL,
+  `remote_orgid` varchar(50) NULL,
+  `remote_orgcode` varchar(20) NULL,
+  `remote_orgclass` varchar(20) NULL,
+  `sender_objid` varchar(50) NULL,
+  `sender_name` varchar(150) NULL,
+  `fileid` varchar(255) NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_reftype` (`reftype`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orgid` (`orgid`),
+  KEY `ix_dtfiled` (`dtfiled`),
+  KEY `ix_fileid` (`fileid`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_state` (`state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+; 
+
+CREATE TABLE `syncdata_item` (
+  `objid` varchar(50) NOT NULL,
+  `parentid` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(255) NOT NULL,
+  `refno` varchar(50) NULL,
+  `action` varchar(100) NOT NULL,
+  `error` text,
+  `idx` int(255) NOT NULL,
+  `info` text,
+  `async` int(11) DEFAULT '0',
+  `dependedaction` varchar(100) NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_parentid` (`parentid`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_state` (`state`),
+  CONSTRAINT `fk_syncdataitem_syncdata` FOREIGN KEY (`parentid`) REFERENCES `syncdata` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `syncdata_pending` (
+  `objid` varchar(50) NOT NULL,
+  `error` text,
+  `expirydate` datetime DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_expirydate` (`expirydate`),
+  CONSTRAINT `fk_syncdata_pending_syncdata` FOREIGN KEY (`objid`) REFERENCES `syncdata` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `syncdata_forprocess` (
+  `objid` varchar(50) NOT NULL,
+  `processed` int(11) DEFAULT '0',
+  PRIMARY KEY (`objid`),
+  CONSTRAINT `fk_forprocess_syncdata_item` FOREIGN KEY (`objid`) REFERENCES `syncdata_item` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `syncdata_forsync` (
+  `objid` varchar(50) NOT NULL,
+  `reftype` varchar(100) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(100) NOT NULL,
+  `orgid` varchar(25) NOT NULL,
+  `dtfiled` datetime NOT NULL,
+  `createdby_objid` varchar(50) NULL,
+  `createdby_name` varchar(255) NULL,
+  `createdby_title` varchar(100) NULL,
+  `info` text,
+  PRIMARY KEY (`objid`),
+  KEY `ix_dtfiled` (`dtfiled`),
+  KEY `ix_createdbyid` (`createdby_objid`),
+  KEY `ix_reftype` (`reftype`),
+  KEY `ix_refno` (`refno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `syncdata_org` (
+  `orgid` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `errorcount` int(11) DEFAULT '0',
+  PRIMARY KEY (`orgid`),
+  KEY `ix_state` (`state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+; 
+
+
+
